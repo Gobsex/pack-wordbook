50be1089458689b892081557d3b3a598cbacc54b
Gobsex <a.golddog15@gmail.com>
2020-06-08 22:47:27 +0300

    Init

---
+++ src/test/java/com/main/packme/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.packme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
